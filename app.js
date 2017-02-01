241860505f8aa90c057ce021200dca2a6012993a
Surya Dutta <suryabrata.dutta@yale.edu>
2017-01-31 21:21:15 -0500

    Adding Listing Page

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require("passport-cas")
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var listings = require('./routes/listings');
 
 var app = express();
 
@@ -22,7 +23,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/users', users);
+app.use('/listings', listings);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
