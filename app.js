5964fedf4d8ddff19d60e73914f2b8f739cf4cd7
Surya Dutta <suryabrata.dutta@yale.edu>
2017-01-31 21:25:15 -0500

    Fix dependency mistake

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var passport = require("passport-cas")
 
 var index = require('./routes/index');
 var listings = require('./routes/listings');
