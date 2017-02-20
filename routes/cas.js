aec2d81b424a50033cf20805b0c999b6fceaba21
Surya Dutta <suryabrata.dutta@yale.edu>
2017-02-20 01:49:15 -0500

    validate CAS ticket

--- routes/cas.js
+++ routes/cas.js
@@ -1,16 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  var CASticket = req.query.ticket;
-  if (CASticket.length!=46){
-    res.send("Boo no ticket!");
-  }
-  else{
-    console.log(req.query.ticket);
-    res.redirect('https://secure.its.yale.edu/cas/validate?service=https://yura-rdb.herokuapp.com/cas&ticket='+req.query.ticket);
-  }
+   request('https://secure.its.yale.edu/cas/validate?service=https://yura-rdb.herokuapp.com/cas&ticket='+req.query.ticket, function (error, response, body) {
+   if (!error && response.statusCode == 200) {
+     console.log(body)
+   }
+ });
+ res.send('hi');
 });
 
 module.exports = router;
