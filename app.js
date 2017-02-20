76166605c3140d20c6fdf9efc16573dc3ce31928
Surya Dutta <suryabrata.dutta@yale.edu>
2017-02-20 11:43:40 -0500

    finalize CAS route with listings

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
-app.use('/cas', cas);
+//app.use('/cas', cas);
 app.use('/listings', listings);
 
 
--- routes/listings.js
+++ routes/listings.js
@@ -4,17 +4,18 @@ var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    if (req.query.ticket){
-      request('https://secure.its.yale.edu/cas/validate?service=https://yura-rdb.herokuapp.com/listings&ticket=' + req.query.ticket, function(error, response, body) {
-          if (!error && response.statusCode == 200 && body.indexOf('yes') !== -1) {
-              res.render('listings', {
-                  title: 'Listings'
-              });
-          } else {
-              res.send('NO ticket');          }
-      });
+    if (req.query.ticket) {
+        request('https://secure.its.yale.edu/cas/validate?service=https://yura-rdb.herokuapp.com/listings&ticket=' + req.query.ticket, function(error, response, body) {
+            if (!error && response.statusCode == 200 && body.indexOf('yes') !== -1) {
+                res.render('listings', {
+                    title: 'Listings'
+                });
+            } else {
+                res.redirect('../');
+            }
+        });
     } else {
-      res.send('no ticket');
+      res.redirect('../');
     }
 });
 
