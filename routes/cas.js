9e54205290c0c6ba21d73a49a3113a55c32e8bb7
Surya Dutta <suryabrata.dutta@yale.edu>
2017-02-20 01:59:08 -0500

    test positive redirect

--- routes/cas.js
+++ routes/cas.js
@@ -5,8 +5,7 @@ var request = require('request');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
    request('https://secure.its.yale.edu/cas/validate?service=https://yura-rdb.herokuapp.com/cas&ticket='+req.query.ticket, function (error, response, body) {
-   if (!error && response.statusCode == 200) {
-     console.log(body);
+   if (!error && response.statusCode == 200 && body.indexOf('yes')!== -1) {
      res.redirect('/listings');
    }
    else{
