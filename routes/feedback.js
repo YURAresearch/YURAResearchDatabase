30fc6420c99da5a9808c91cfa4fe71647742666e
Surya <suryabrata.dutta@yale.edu>
2017-09-30 22:52:17 -0400

    initialize mailing feedback service

--- routes/feedback.js
+++ routes/feedback.js
@@ -1,44 +1,32 @@
 var express = require('express');
 var router = express.Router();
-const nodemailer = require('nodemailer');
+var Mailgun = require('mailgun').Mailgun;
+
+var mg = new Mailgun('key-5232d6c7aa1fdebccd7e1b32cf328506');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('feedback', {
-        title: 'Feedback'
-    });
+  res.render('feedback', {
+    title: 'Feedback'
+  });
 });
 
 router.post('/', function(req, res) {
-    if (req.query.submit) {
-        var transporter = nodemailer.createTransport('smtps://yura.database.feedback%40gmail.com:undergradresearch@smtp.gmail.com');
-
-        // setup email data with unicode symbols
-        var mailOptions = {
-            from:'yura.database.feedback@gmail.com', // sender address
-            to: 'yura.database@gmail.com', // list of receivers
-            replyTo: req.body.email,
-            subject: '[RDB Feedback] '+req.body.subject, // Subject line
-            html: '<h3>Name</h3><p>' + req.body.name + '</p><h1>Message</h1><p>' + req.body.message + '</p>', // plain text body
-        };
-
-        // send mail with defined transport object
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                res.render('feedback', {
-                    title: 'Feedback',
-                    message: 'Error: Please try again, or email us at yura@yale.edu.',
-                });
-            } else {
-                res.render('feedback', {
-                    title: 'Feedback',
-                    message: 'Your message has been sent! Thank you for your feedback.',
-                });
-            }
+  mg.sendText(req.body.email, 'yura.database@gmail.com',
+    req.body.subject,req.body.message,
+    function(err) {
+      if (err) {
+        res.render('feedback', {
+          title: 'Feedback',
+          message: 'Error: Please try again, or email us at yura@yale.edu.',
         });
-    } else {
-        res.send('Error');
-    }
+      } else {
+        res.render('feedback', {
+          title: 'Feedback',
+          message: 'Your message has been sent! Thank you for your feedback.',
+        });
+      }
+    });
 });
 
 module.exports = router;
