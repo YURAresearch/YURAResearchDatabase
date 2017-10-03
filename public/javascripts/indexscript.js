a67a68b62911ac468407f610049867b58afef282
Peter Wang <ptrw08@gmail.com>
2017-10-03 14:50:43 -0400

    Clean up plugins, reroute page-specific js & css

--- public/javascripts/indexscript.js
+++ public/javascripts/indexscript.js
@@ -59,25 +59,23 @@ function changeTestimonial() {
 
 // All main scripts
 $(document).ready(function() {
-    if ($('#indexmain').length === 0) {
-        // Learn more button
-        $('#learnmore').localScroll({duration:800});
+    // Learn more button
+    $('#learnmore').localScroll({duration:800});
 
-        // Scroll to top button
-        $('#back-top').hide();
-        $(window).scroll(function() {
-            if(($(this).scrollTop() > 900) && ((window.innerHeight + window.scrollY) < document.body.scrollHeight)){
-                $('#back-top').fadeIn(300);
-            }
-            else{
-                $('#back-top').fadeOut(300);
-            }
-        });
-        $('#back-top').click(function() {
-            $('body,html').animate({scrollTop:0}, 400);
-        });
+    // Scroll to top button
+    $('#back-top').hide();
+    $(window).scroll(function() {
+        if(($(this).scrollTop() > 900) && ((window.innerHeight + window.scrollY) < document.body.scrollHeight)){
+            $('#back-top').fadeIn(300);
+        }
+        else{
+            $('#back-top').fadeOut(300);
+        }
+    });
+    $('#back-top').click(function() {
+        $('body,html').animate({scrollTop:0}, 400);
+    });
 
-        // Testimonials
-        setInterval(changeTestimonial, 3000);
-    }
+    // Testimonials
+    setInterval(changeTestimonial, 3000);
 });
