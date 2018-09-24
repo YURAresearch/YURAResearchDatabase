9cab90f212dd230fdb375a4d9de73295e5bd6b6a
Surya Dutta <suryabrata.dutta@yale.edu>
2018-09-24 11:18:49 -0500

    add savesearch config var

--- bin/config.js
+++ bin/config.js
@@ -5,6 +5,8 @@ config.host = process.env.HOST || 'localhost';
 config.sessionSecret = process.env.SESSION_SECRET || 'e70a1e1ee4b8f662f78'
 config.mailgun_key = process.env.MAILGUN_KEY
 
+config.save_search = false;
+
 config.cookieduration = 1000 * 60 * 60 * 24; //one hour
 
 module.exports = config;
